721b62bb4d4214468d033af60e4601068ee724cd
Savitha M <savitha@Savithas-MacBook-Air.local>
2023-11-26 01:14:53 +0530

    crud and ui designed

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const path=require("path");
 const Listing=require("./models/listing.js")
 const { v4: uuidv4 } = require('uuid');
 var methodOverride = require('method-override')
+const ejsmate = require('ejs-mate')
 
 app.use(express.static(path.join(__dirname,"public")));
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.engine('ejs', ejsmate);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 const mongoose = require('mongoose');
@@ -43,22 +45,41 @@ app.get("/listings/new", (req, res) => {
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    console.log(listing.image.url)
     res.render("listings/show.ejs", { listing });
   });
 
 
 //Create Route
 app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
+  const newListing = new Listing({
+    title: req.body.listing.title,
+    description: req.body.listing.description,
+    image: req.body.listing.image, // Assuming 'image' is a field in your Listing model
+    price: req.body.listing.price,
+    country: req.body.listing.country,
+    location: req.body.listing.location
+    // Add other fields as needed
+  });
+console.log(newListing);
+  try {
     await newListing.save();
     res.redirect("/listings");
-  });
+  } catch (err) {
+    // Handle error
+    console.error(err);
+    res.status(500).send("Error creating listing");
+  }
+});
 
+// Assuming your Listing model schema looks like this:
+
+
+   
   //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
-  console.log(listing)
   res.render("listings/edit.ejs", { listing });
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
-console.log("js running")
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('click', function(event) {
+      if (event.target && event.target.id === 'darkModeToggle') {
+        toggleDarkMode();
+      }
+    });
+  });
+  
+  function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+  }
